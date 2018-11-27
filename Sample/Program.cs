40379cf0aadf71af2dd4397cbe87caf7a632da50
Rene Rotvig  Jensen <renerotvig@msn.com>
2018-11-27 21:40:10 +0100

    Upgraded to netstandard 2.0

---
+++ Sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using LightWebNetScraper;
+using System;
+using System.Collections.Generic;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scraper = new Scraper();
+
+            var result = scraper.StartScraping(new List<ScrapeInfo<string>>()
+                {
+                    new ScrapeInfo<string>(
+                        "https://www.nuget.org/packages/LightWebNetScraper",
+                        (htmlDoc, scrapeInfo) =>  htmlDoc.DocumentNode.SelectSingleNode(@"//div[@class='package-title']/h1").InnerHtml)
+                    });
+            Console.WriteLine(result[0]);
+            Console.ReadLine();
+        }
+    }
+}
